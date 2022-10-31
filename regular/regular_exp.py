2adc1f81f34aa63cb1c9fe27eece4384589e8b77
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-31 15:31:33 +0600

    finally it replaces phone numbers in right way

--- regular/regular_exp.py
+++ regular/regular_exp.py
@@ -12,18 +12,27 @@ with open("phonebook_raw.csv") as f:
 # TODO 1: выполните пункты 1-3 ДЗ
 phone_pattern = r"(\+7|8)?\s*[(]?(\d{3})[)]?\s?[-]?(\d{3})[-]*(\d{2})[-]*(\d{2})\s?\(?([доб.]{4})?\s?(\d{4})?"
 sub_pattern = r"+7(\2)\3-\4-\5 \6\7"
-phones = []
-for line in contacts_list:
-    for i in range(len(line)):
-        if re.findall(phone_pattern, line[i]):
-            phones.append(re.findall(phone_pattern, line[i]))
 
-for line in phones:
-    for item in line:
-        if item[-2] == '':
-            line.pop(0)
-            line.append(item[:-2])
-pprint(phones)
+for line in contacts_list:
+    changed_line = re.sub(phone_pattern, sub_pattern, line[-2])
+    line[-2] = changed_line
+pprint(contacts_list)
+
+phones = []
+    # for element in line:
+    #     if re.search(phone_pattern, element):
+    #         print('yes')
+    #         re.sub(phone_pattern, 'asshole', element)
+    #         print(element)
+    # #     phones.append(re.search(phone_pattern, line))
+
+# for line in phones:
+#     for item in line:
+#         if item[-2] == '':
+#             line.pop(0)
+#             line.append(item[:-2])
+# pprint(phones)
+# pprint(contacts_list)
 
 
 # TODO 2: сохраните получившиеся данные в другой файл
