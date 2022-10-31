60b72a365dbb5768e16dae02d2cdb67026f8ca43
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-31 10:41:11 +0600

    no substitution only phone list fetch

--- regular/regular_exp.py
+++ regular/regular_exp.py
@@ -11,33 +11,20 @@ with open("phonebook_raw.csv") as f:
 
 # TODO 1: выполните пункты 1-3 ДЗ
 phone_pattern = r"(\+7|8)?\s*[(]?(\d{3})[)]?\s?[-]?(\d{3})[-]*(\d{2})[-]*(\d{2})\s?\(?([доб.]{4})?\s?(\d{4})?"
-# for line in contacts_list:
-#
-#     re.sub(phone_pattern, r"+7(\2)\3-\4-\5 \6\7", line)
-#     # pprint(item)
-
+sub_pattern = r"+7(\2)\3-\4-\5 \6\7"
 phones = []
 for line in contacts_list:
+    for i in range(len(line)):
+        if re.findall(phone_pattern, line[i]):
+            phones.append(re.findall(phone_pattern, line[i]))
+
+for line in phones:
     for item in line:
-        for i in range(len(item)):
-            if re.findall(phone_pattern, item[i]):
-                re.sub(phone_pattern, r"+7(\2)\3-\4-\5 \6\7", item[i])
-                phones.append(re.findall(phone_pattern, item[i]))
+        if item[-2] == '':
+            line.pop(0)
+            line.append(item[:-2])
+pprint(phones)
 
-# pprint(contacts_list)
-# for line in phones:
-#     for item in line:
-#         if item[-2] == '':
-#             line.pop(0)
-#             line.append(item[:-2])
-# pprint(phones)
-
-for line in contacts_list:
-    for item in line:
-
-        re.sub(phone_pattern, r"+7(\2)\3-\4-\5 \6\7", item)
-        phones.append(re.findall(phone_pattern, item))
-pprint(contacts_list)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
