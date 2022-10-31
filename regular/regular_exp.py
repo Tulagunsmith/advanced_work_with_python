f2d41180de8e8cc55ed946b5c292ac1adecbc2b5
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-31 16:54:29 +0600

    person name fixed

--- regular/regular_exp.py
+++ regular/regular_exp.py
@@ -13,27 +13,20 @@ with open("phonebook_raw.csv") as f:
 phone_pattern = r"(\+7|8)?\s*[(]?(\d{3})[)]?\s?[-]?(\d{3})[-]*(\d{2})[-]*(\d{2})\s?\(?([доб.]{4})?\s?(\d{4})?"
 sub_pattern = r"+7(\2)\3-\4-\5 \6\7"
 
+name_pattern = r"([А-Я]\w+)(.?)([А-Я]\w+)(.?)([А-Я]\w+)?"
+sub_name_pattern = r"\1 \3 \5 "
+
 for line in contacts_list:
-    changed_line = re.sub(phone_pattern, sub_pattern, line[-2])
-    line[-2] = changed_line
+    changed_phone_line = re.sub(phone_pattern, sub_pattern, line[-2])
+    line[-2] = changed_phone_line
+
+    line_name = ' '.join(line[0:3])
+    changed_name_line = re.sub(name_pattern, sub_name_pattern, line_name)
+    changed_name_line = changed_name_line.split()
+
+    for i in range(len(changed_name_line)):
+        line[i] = changed_name_line[i]
 pprint(contacts_list)
-
-phones = []
-    # for element in line:
-    #     if re.search(phone_pattern, element):
-    #         print('yes')
-    #         re.sub(phone_pattern, 'asshole', element)
-    #         print(element)
-    # #     phones.append(re.search(phone_pattern, line))
-
-# for line in phones:
-#     for item in line:
-#         if item[-2] == '':
-#             line.pop(0)
-#             line.append(item[:-2])
-# pprint(phones)
-# pprint(contacts_list)
-
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
