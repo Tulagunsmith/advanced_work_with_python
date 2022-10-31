65b59ca6a1c5f20f82b1aa217d73f8921f8c6019
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-31 20:34:54 +0600

    organization fix added

--- regular/regular_exp.py
+++ regular/regular_exp.py
@@ -16,6 +16,9 @@ sub_pattern = r"+7(\2)\3-\4-\5 \6\7"
 name_pattern = r"([А-Я]\w+)(.?)([А-Я]\w+)(.?)([А-Я]\w+)?"
 sub_name_pattern = r"\1 \3 \5 "
 
+organization_pattern = r"[,]([А-Я]{3})[,]|[,]([А-Я]\w+)[,]\W"
+sub_organization_pattern = r" \1\2 "
+
 for line in contacts_list:
     changed_phone_line = re.sub(phone_pattern, sub_pattern, line[-2])
     line[-2] = changed_phone_line
@@ -26,6 +29,9 @@ for line in contacts_list:
 
     for i in range(len(changed_name_line)):
         line[i] = changed_name_line[i]
+
+    changed_organization_line = re.sub(organization_pattern, sub_organization_pattern, line[3])
+    line[3] = changed_organization_line
 pprint(contacts_list)
 
 # TODO 2: сохраните получившиеся данные в другой файл
