a764faeabcf5b81a8bbe47709a68133e1013be1d
Tulagunsmith <tulagunsmith@gmail.com>
2022-11-06 14:16:15 +0600

    entry point added, functions added

--- web_scraping/scraping.py
+++ web_scraping/scraping.py
@@ -1,6 +1,7 @@
 import bs4
 import requests
 
+
 url = 'https://habr.com/ru/'
 HEADERS = {
     'Accept': '*/*', 'Connection': 'keep-alive',
@@ -9,29 +10,39 @@ HEADERS = {
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'android', 'sitemap', ]
 
-response = requests.get(url, headers=HEADERS)
-text = response.text
-soup = bs4.BeautifulSoup(text, features='html.parser')
 
-articles = soup.find_all('article')
-link_list = []
-for article in articles:
-    article_titles = article.find_all(class_="tm-article-snippet__title tm-article-snippet__title_h2")
-    article_titles = [title.text.strip().lower() for title in article_titles]
-    article_tags = article.find_all(class_="tm-article-snippet__hubs-item")
-    article_tags = [tag.text.strip().lower() for tag in article_tags]
-    article_bodies = article.find_all(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
-    article_bodies = [body.text.strip().lower() for body in article_bodies]
-    article_text = (*article_titles, *article_tags, *article_bodies)
-    for line in article_text:
-        line = line.split()
-        for element in line:
-            if element in KEYWORDS:
-                date = article.find(class_='tm-article-snippet__datetime-published').contents[0]['title']
-                title = article.find(class_="tm-article-snippet__title-link").text
-                href = article.find(class_='tm-article-snippet__title-link').attrs['href']
-                link = f'{"https://habr.com"}{href}'
-                if title not in link_list:
-                    link_list.append(title)
-                    print(f'{date} -> {title} ==> {link}')
+def get_data(site_address, head):
+    response = requests.get(site_address, headers=head)
+    text = response.text
+    soup = bs4.BeautifulSoup(text, features='html.parser')
+    articles = soup.find_all('article')
+    return articles
 
+
+def get_news(data, search_words):
+
+    link_list = []
+
+    for article in data:
+        article_titles = article.find_all(class_="tm-article-snippet__title tm-article-snippet__title_h2")
+        article_titles = [title.text.strip().lower() for title in article_titles]
+        article_tags = article.find_all(class_="tm-article-snippet__hubs-item")
+        article_tags = [tag.text.strip().lower() for tag in article_tags]
+        article_bodies = article.find_all(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
+        article_bodies = [body.text.strip().lower() for body in article_bodies]
+        article_text = (*article_titles, *article_tags, *article_bodies)
+        for line in article_text:
+            line = line.split()
+            for element in line:
+                if element in search_words:
+                    date = article.find(class_='tm-article-snippet__datetime-published').contents[0]['title']
+                    title = article.find(class_="tm-article-snippet__title-link").text
+                    href = article.find(class_='tm-article-snippet__title-link').attrs['href']
+                    link = f'{"https://habr.com"}{href}'
+                    if title not in link_list:
+                        link_list.append(title)
+                        print(f'{date} -> {title} ==> {link}')
+
+
+if __name__ == '__main__':
+    get_news(get_data(url, HEADERS), KEYWORDS)
