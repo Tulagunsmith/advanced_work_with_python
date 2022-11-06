9a74522252d5d4d9d3ad0fe848457a147876ab9a
Tulagunsmith <tulagunsmith@gmail.com>
2022-11-06 14:01:41 +0600

    final logic added

--- web_scraping/scraping.py
+++ web_scraping/scraping.py
@@ -1,6 +1,5 @@
 import bs4
 import requests
-import datetime
 
 url = 'https://habr.com/ru/'
 HEADERS = {
@@ -8,22 +7,22 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36 OPR/54.0.2952.64'}
 
-KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'Android', 'sitemap']
+KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'android', 'sitemap', ]
 
 response = requests.get(url, headers=HEADERS)
-print(response.status_code)
 text = response.text
 soup = bs4.BeautifulSoup(text, features='html.parser')
 
 articles = soup.find_all('article')
+link_list = []
 for article in articles:
     article_titles = article.find_all(class_="tm-article-snippet__title tm-article-snippet__title_h2")
-    article_titles = [title.text.strip() for title in article_titles]
+    article_titles = [title.text.strip().lower() for title in article_titles]
     article_tags = article.find_all(class_="tm-article-snippet__hubs-item")
-    article_tags = [tag.text.strip() for tag in article_tags]
+    article_tags = [tag.text.strip().lower() for tag in article_tags]
     article_bodies = article.find_all(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
-    article_bodies = [body.text.strip() for body in article_bodies]
-    article_text = [*article_titles, *article_tags, *article_bodies]
+    article_bodies = [body.text.strip().lower() for body in article_bodies]
+    article_text = (*article_titles, *article_tags, *article_bodies)
     for line in article_text:
         line = line.split()
         for element in line:
@@ -32,5 +31,7 @@ for article in articles:
                 title = article.find(class_="tm-article-snippet__title-link").text
                 href = article.find(class_='tm-article-snippet__title-link').attrs['href']
                 link = f'{"https://habr.com"}{href}'
-                print(title)
+                if title not in link_list:
+                    link_list.append(title)
+                    print(f'{date} -> {title} ==> {link}')
 
