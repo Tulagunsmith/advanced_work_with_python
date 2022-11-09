2985a355ea4e7167cbc8cc263f498098d9e6e325
Tulagunsmith <tulagunsmith@gmail.com>
2022-11-09 23:35:28 +0600

    logic improved

--- iterators_generators/generator_task.py
+++ iterators_generators/generator_task.py
@@ -3,23 +3,17 @@ import types
 
 def flat_generator(list_of_lists):
     copy_list = list_of_lists[:]
-    # print(copy_list)
     flag = True
     while flag:
         new_list = []
         flag = False
         for i in copy_list:
-            # print(f' for {new_list}')
             if isinstance(i, list):
                 new_list.extend(i)
-                # print(f' instanse {new_list}')
                 flag = True
             else:
-                new_list.append(i)
-        # print(new_list)
+                yield i
         copy_list = new_list
-
-    yield copy_list
 
 
 def test_2():
@@ -40,15 +34,5 @@ def test_2():
     assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
 
 
-list_of_lists_1 = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f', 'h', False],
-    [1, 2, None]
-]
 if __name__ == '__main__':
-    a = flat_generator(list_of_lists_1)
-    for item in a:
-        print(item)
-    # print(flat_generator(list_of_lists_1))
-    # print(isinstance(flat_generator(list_of_lists_1), types.GeneratorType))
     test_2()
